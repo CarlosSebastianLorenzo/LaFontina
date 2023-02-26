e93d98e437cd5b4e9689f7cbf23fc0078c0c2526
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2023-02-26 14:30:23 -0300

    Creación y estilado del componente menú Hamburguesa

---
+++ src/Components/BurguerMenu.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import './BurguerMenu.css'
+
+function BurguerMenu({fn, condition}) {
+
+    return (
+        <div className="burguer" onClick={()=> fn()}>
+            <span className={condition ? 'burguerTop' : ''}></span>
+            <span className={condition ? 'burguerMiddle' : ''}></span>
+            <span className={condition ? 'burguerBottom' : ''}></span>
+        </div>
+    )
+}
+
+export default BurguerMenu
