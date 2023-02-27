7f17c6cd6ba4f16cc4c370b9a0893c1b979aae61
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2023-02-27 14:30:23 -0300

    Mínimo estilado de header y agregado de links

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,20 +1,25 @@
 import './Header.css'
+import { AiOutlineShoppingCart, AiOutlineWhatsApp, AiOutlinePhone } from "react-icons/ai";
+import { Link } from 'react-router-dom'
 
 const Header = () => {
 
     return (
         <header>
-            <div>
+            <Link to='/'>
                 <img src="../../public/laFontinaLogo.jpg" alt="La Fontina" />
                 <h1>La Fontina</h1>
-            </div>
+            </Link>
             <nav>
                 <ul>
                     <li>
-                        <a href="#">lugar 1</a>
+                        <Link className='link' target='_blank' to="tel:+54-9-351-508-1999"><AiOutlinePhone/>351-508-1999</Link>
                     </li>
                     <li>
-                        <a href="#">lugar 2</a>
+                        <Link className='link' target='_blank' to="https://api.whatsapp.com/send?phone=5493515081999&text="><AiOutlineWhatsApp/>351-508-1999</Link>
+                    </li>
+                    <li>
+                        <Link to="#"><AiOutlineShoppingCart/></Link>
                     </li>
                 </ul>
             </nav>
