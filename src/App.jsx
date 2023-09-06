d2bff2465f6cce16976404a53cc74c95aa0e9ee9
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2023-09-06 14:15:37 -0300

    Enrutamiento, Fetch de Productos por Params, Mapeo de botones que filtran por categoría y lógica

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const router = createBrowserRouter([
         element: <Products/>,
       },
       {
-        path: '/detalle/:name',
+        path: '/:name/:product',
         element: <Details/>,
       }
     ]
--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -9,15 +9,34 @@ const Products = () => {
     const params = useParams()
     const [productsArray, setArray] = useState([])
     const [filterProducts, setFilterProducts] = useState([])
+    const [categoriesArray, setCategoriesArray] = useState([])
     useEffect(()=>{
-        fetch('src/Utils/Produtcts.json')
+        fetch('src/Utils/Products.json')
         .then((response) => response.json())
         .then(data => {
-            setArray(data[`${params.name}`])
-            setFilterProducts(data[`${params.name}`])
+            const dataArray = data[`${params.name}`]
+            if (dataArray == undefined) { 
+                setArray([])
+                setFilterProducts([])
+                setCategoriesArray([])
+                return
+            }
+            setArray(dataArray)
+            setFilterProducts(dataArray)
+            const Categories = dataArray.map(e => e.category)
+            setCategoriesArray([...new Set(Categories)])
         })
         .catch(err => console.error(err))
     },[params])
+
+    const filterByCategory = (category) => {
+        if(category == "All") {setFilterProducts(productsArray)
+        }  
+        else{
+            let filteredArray = productsArray.filter(e => e.category == category)
+            setFilterProducts(filteredArray)
+        }
+    }
 
     const handleSearch = (e)=> {
         
@@ -35,8 +54,18 @@ const Products = () => {
     return (
         <main className="products">
             <div>
-                <input type="text" name="search" placeholder=" " onChange={(e)=>handleSearch(e.target.value)}/>
-                <label htmlFor="search">Filtra por Ingrediente</label>
+                <div>
+                    <input type="text" name="search" placeholder=" " onChange={(e)=>handleSearch(e.target.value)}/>
+                    <label htmlFor="search">Filtra por Ingredientes</label>
+                </div>
+                <div>
+                    <button className="category" onClick={()=>filterByCategory("All")}>Todos</button>
+                { 
+                    categoriesArray.map((elem, index) => {
+                        return <button className="category" key={index} onClick={()=>filterByCategory(elem)}>{elem}</button>
+                    })
+                }
+                </div>
             </div>
             <div>
                 {
@@ -49,7 +78,7 @@ const Products = () => {
                     </>
                     :
                     filterProducts.map((product, indexMap) => {
-                    return <Link to={'/detalle/'+product.title} key={indexMap}>
+                    return <Link to={'/'+params.name+'/'+product.title} key={indexMap}>
                         <Card data={product}></Card>
                     </Link>
                 })
